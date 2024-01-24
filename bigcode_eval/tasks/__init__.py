fb0e434f00e8da3864b6e2dc8f8e3cf5e77955fe
Phuong Le <hpcqt97@gmail.com>
2024-01-24 23:23:43 +0700

    feat: add code_to_code_trans

--- bigcode_eval/tasks/__init__.py
+++ bigcode_eval/tasks/__init__.py
@@ -1,40 +1,21 @@
 import inspect
 from pprint import pprint
 
-from . import (apps, codexglue_code_to_text, codexglue_text_to_text, conala,
-               concode, ds1000, gsm, humaneval, humanevalpack,
-               instruct_humaneval, instruct_wizard_humaneval, mbpp, multiple, multiple_enc_dec,
-               parity, python_bugs, quixbugs, recode, santacoder_fim)
+from . import code_to_code
 
 TASK_REGISTRY = {
-    **apps.create_all_tasks(),
-    **codexglue_code_to_text.create_all_tasks(),
-    **codexglue_text_to_text.create_all_tasks(),
-    **multiple.create_all_tasks(),
-    **multiple_enc_dec.create_all_tasks(),
-    "codexglue_code_to_text-python-left": codexglue_code_to_text.LeftCodeToText,
-    "conala": conala.Conala,
-    "concode": concode.Concode,
-    **ds1000.create_all_tasks(),
-    **humaneval.create_all_tasks(),
-    **humanevalpack.create_all_tasks(),
-    "mbpp": mbpp.MBPP,
-    "parity": parity.Parity,
-    "python_bugs": python_bugs.PythonBugs,
-    "quixbugs": quixbugs.QuixBugs,
-    "instruct_wizard_humaneval": instruct_wizard_humaneval.HumanEvalWizardCoder,
-    **gsm.create_all_tasks(),
-    **instruct_humaneval.create_all_tasks(),
-    **recode.create_all_tasks(),
-    **santacoder_fim.create_all_tasks(),
+    **code_to_code.multiple.create_all_tasks(),
 }
 
-ALL_TASKS = sorted(list(TASK_REGISTRY))
+TRANSLATION_TASKS = TASK_REGISTRY
+
+ALL_TASKS = sorted(list(TRANSLATION_TASKS))
 
 
 def get_task(task_name, args=None):
     try:
-        kwargs = {}
+        kwargs = {'source_lang': args.source_lang,
+                  'source_generations_path': args.source_generations_path}
         if "prompt" in inspect.signature(TASK_REGISTRY[task_name]).parameters:
             kwargs["prompt"] = args.prompt
         if "load_data_path" in inspect.signature(TASK_REGISTRY[task_name]).parameters:
