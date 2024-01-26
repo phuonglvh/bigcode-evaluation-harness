00acb4782685367b0d86a7eea10e2920809d3084
Phuong Le <hpcqt97@gmail.com>
2024-01-26 14:43:51 +0700

    feat: install javatuples

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -4,3 +4,6 @@ apt-get update -y
 apt install -y openjdk-11-jdk-headless
 java --version
 javac --version
+
+# install mvn javatuples
+mkdir /usr/multiple && wget https://repo.mavenlibs.com/maven/org/javatuples/javatuples/1.2/javatuples-1.2.jar -O /usr/multiple/javatuples-1.2.jar
