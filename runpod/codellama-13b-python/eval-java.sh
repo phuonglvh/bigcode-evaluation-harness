45381d9aa8820d9ca0b742a4dc44874f1e1e11da
Phuong Le <hpcqt97@gmail.com>
2024-01-26 16:41:21 +0700

    feat: update eval-java.sh

--- runpod/codellama-13b-python/eval-java.sh
+++ runpod/codellama-13b-python/eval-java.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euox
+
 AUTHOR="codellama"
 MODEL_NAME="CodeLlama-13b-Python-hf"
 max_length=1024
@@ -10,8 +12,8 @@ n_samples=200
 seed=0
 precision=bf16
 lang=java
-limit_start=0
-limit=158
+# limit_start=0
+# limit=158
 batch_size=10
 
 python main.py --model "$AUTHOR/$MODEL_NAME" \
@@ -27,11 +29,5 @@ python main.py --model "$AUTHOR/$MODEL_NAME" \
     --allow_code_execution \
     --trust_remote_code \
     --save_every_k_tasks 10 \
-    --save_generations \
-    --save_generations_path "./$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations-$limit_start-$limit.json" \
-    --save_references \
-    --generation_only \
-    --limit_start $limit_start \
-    --limit $limit \
-    --load_generations_path "./$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations_multiple-$lang.json" \
-    --metric_output_path "./$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations_multiple-$lang-evaluation_results.json"
+    --load_generations_path "./runpod/codellama-13b-python/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations_multiple-$lang.json" \
+    --metric_output_path "./runpod/codellama-13b-python/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations_multiple-$lang-evaluation_results.json"
