18db1a103cdee4423010c8cb2c947fbe1402fb67
Phuong Le <hpcqt97@gmail.com>
2024-01-27 18:21:54 +0700

    feat: add Mistral-7B-v0.1/java generations

--- runpod/Mistral-7B-v0.1/java/gen-java.sh
+++ runpod/Mistral-7B-v0.1/java/gen-java.sh
@@ -106,4 +106,39 @@ python main.py --model "$AUTHOR/$MODEL_NAME" \
     --save_references \
     --generation_only \
     --limit_start $limit_start \
-    --limit $limit
+    --limit $
+
+
+# concat parts
+AUTHOR="mistralai"
+MODEL_NAME="Mistral-7B-v0.1"
+max_length=1024
+temperature=0.8
+top_p=0.95
+top_k=0
+n_samples=200
+batch_size=20
+seed=0
+precision=bf16
+lang=java
+
+part_1_path="$BASE_DIR/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations-0-50_multiple-$lang.json"
+part_2_path="$BASE_DIR/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations-50-50_multiple-$lang.json"
+part_3_path="$BASE_DIR/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations-100-100_multiple-$lang.json"
+full_gen_path="$BASE_DIR/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations_multiple-$lang.json"
+
+python_concat_script="
+part_1_path = '$part_1_path'
+part_2_path = '$part_2_path'
+part_3_path = '$part_3_path'
+full_gen_path = '$full_gen_path'
+import json
+part_1_data = json.load(open(part_1_path, 'r'))[0:50]
+part_2_data = json.load(open(part_2_path, 'r'))[0:50]
+part_3_data = json.load(open(part_3_path, 'r'))[0:61]
+all_gens = part_1_data + part_2_data + part_3_data
+json.dump(all_gens, open(full_gen_path, 'w'))
+print(f'wrote {len(all_gens)} to \"{full_gen_path}\"')
+"
+
+python -c "$python_concat_script"
