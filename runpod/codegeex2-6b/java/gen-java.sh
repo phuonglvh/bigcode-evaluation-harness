13462a3cb2739d230227a54311843ff136e0ba93
Phuong Le <hpcqt97@gmail.com>
2024-01-27 17:54:43 +0700

    feat: update codegeex2-6b java gen-java.sh

--- runpod/codegeex2-6b/java/gen-java.sh
+++ runpod/codegeex2-6b/java/gen-java.sh
@@ -112,3 +112,36 @@ python main.py --model "$AUTHOR/$MODEL_NAME" \
     --token \
     --limit_start $limit_start \
     --limit $limit
+
+# FULL
+AUTHOR="THUDM"
+MODEL_NAME="codegeex2-6b"
+max_length=1024
+temperature=0.8
+top_p=0.95
+top_k=0
+n_samples=200
+batch_size=20
+seed=0
+precision=bf16
+lang=java
+save_every_k_tasks=50
+
+python main.py --model "$AUTHOR/$MODEL_NAME" \
+    --tasks multiple-$lang \
+    --max_length_generation $max_length \
+    --temperature $temperature \
+    --top_p $top_p \
+    --top_k $top_k \
+    --seed $seed \
+    --n_samples $n_samples \
+    --batch_size $batch_size \
+    --precision $precision \
+    --allow_code_execution \
+    --trust_remote_code \
+    --save_every_k_tasks $save_every_k_tasks \
+    --save_generations \
+    --save_generations_path "$BASE_DIR/$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang-generations.json" \
+    --save_references \
+    --generation_only \
+    --token
