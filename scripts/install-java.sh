a3fddf53bb6ddc82fac60bf04155820804a3cb49
phuonglvh <hpcqt97@gmail.com>
2024-07-16 20:40:02 +0700

    feat: correct install-java script

--- scripts/install-java.sh
+++ scripts/install-java.sh
@@ -5,7 +5,7 @@ apt install -y openjdk-11-jdk-headless
 java --version
 javac --version
 
-EVAL_JAVA_EXTRA_CLASSPATH_FOLDER="${EVAL_JAVA_EXTRA_CLASSPATH_FOLDER:-'/usr/multiple'}"
+EVAL_JAVA_EXTRA_CLASSPATH_FOLDER="${EVAL_JAVA_EXTRA_CLASSPATH_FOLDER:-/usr/multiple}"
 
 # install mvn javatuples
 mkdir -p "$EVAL_JAVA_EXTRA_CLASSPATH_FOLDER" && wget https://repo.mavenlibs.com/maven/org/javatuples/javatuples/1.2/javatuples-1.2.jar -O "$EVAL_JAVA_EXTRA_CLASSPATH_FOLDER/javatuples-1.2.jar"
