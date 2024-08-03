8a6e261dcc4a5f138488a5b5c57f32031ec2b48b
phuonglvh <hpcqt97@gmail.com>
2024-08-03 11:25:04 +0700

    feat: add runpod/codellama-13b-python/java/improve/t0.8-p0.95-k0-batch1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/phuonglvh/bigcode-evaluation-harness.git
+cd bigcode-evaluation-harness
 git checkout feature/thesis
 pip install -r requirements.txt
 bash scripts/install-useful-tools.sh
