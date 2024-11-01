9e90c4e691914cf227e9a9897c38a159018d4de4
phuonglvh <hpcqt97@gmail.com>
2024-11-01 23:02:41 +0700

    feat: correct codellama greedy

--- runpod/CodeLlama-13b-Python-hf/py/do_sampleFalse/gen-eval.sh
+++ runpod/CodeLlama-13b-Python-hf/py/do_sampleFalse/gen-eval.sh
@@ -4,17 +4,13 @@ set -euox
 
 AUTHOR="codellama"
 MODEL_NAME="CodeLlama-13b-Python-hf"
-max_length=512
 
+max_length=1024
 do_sample=False
 num_return_sequences=1
 batch_size=$num_return_sequences
-
-BASE_DIR=./runpod/$MODEL_NAME/$lang/do_sample$do_sample
-mkdir -p $BASE_DIR
-
 n_samples=1
-seed=10
+seed=0
 precision=bf16
 lang=py
 
@@ -26,9 +22,12 @@ eval_limit=158
 save_every_k_tasks=1 # after completing k dataset's tasks
 save_every_k_iterations=$((save_every_k_tasks * n_samples / batch_size))
 
-common_name="$MODEL_NAME-do_sample$do_sample-$precision-n$n_samples-maxlen$max_length-$lang"
+common_name="$MODEL_NAME-do_sample$do_sample-$precision-n$n_samples-seed$seed-maxlen$max_length-$lang"
 generations_name="$common_name-generations-${limit_start}-${limit}_multiple-$lang"
-generations_path="$BASE_DIR/$generations_name.json"
+
+BASE_DIR=./runpod/$MODEL_NAME/$lang/do_sample$do_sample
+mkdir -p $BASE_DIR
+rm -rf /tmp/* /var/tmp/*
 
 python main.py --model "$AUTHOR/$MODEL_NAME" \
     --tasks multiple-$lang \
@@ -48,21 +47,3 @@ python main.py --model "$AUTHOR/$MODEL_NAME" \
     --limit $limit \
     --metric_output_path "$BASE_DIR/$generations_name-eval-${eval_limit_start}-${eval_limit}-evaluation_results.json" \
     --max_memory_per_gpu auto
-
-rm -rf /tmp/* /var/tmp/*
-
-python main.py --model "$AUTHOR/$MODEL_NAME" \
-    --tasks multiple-$lang \
-    --max_length_generation $max_length \
-    --do_sample $do_sample \
-    --seed $seed \
-    --n_samples $n_samples \
-    --batch_size $batch_size \
-    --precision $precision \
-    --allow_code_execution \
-    --trust_remote_code \
-    --limit_start $eval_limit_start \
-    --limit $eval_limit \
-    --save_every_k_tasks $save_every_k_iterations \
-    --load_generations_path "$generations_path" \
-    --metric_output_path "$BASE_DIR/$generations_name-eval-${eval_limit_start}-${eval_limit}-evaluation_results.json"
