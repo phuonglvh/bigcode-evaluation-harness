38d31e521f355a7d475f678b82f80cd9e00eef27
phuonglvh <hpcqt97@gmail.com>
2024-11-02 17:08:49 +0700

    feat: move folder

--- runpod/starcoder/java/improve/t0.8-p0.95-k0-batch20-n200/gen-eval.sh
+++ runpod/starcoder/java/improve/t0.8-p0.95-k0-batch20-n200/gen-eval.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-BASE_DIR="${BASE_DIR:-.}"
 AUTHOR="bigcode"
 MODEL_NAME="starcoder"
-max_length=512
+
+max_length=1024
 temperature=0.8
-top_p=0.95
 top_k=0
+top_p=0.95
+num_return_sequences=10
+batch_size=$num_return_sequences
+
 n_samples=200
 seed=0
 precision=bf16
-lang=py
-full_language=python
-# limit_start=0
-# limit=164
-batch_size=100
+lang=java
 
-save_every_k_tasks=5 # after completing 5 dataset's tasks
+limit_start=0
+limit=158
+eval_limit_start=0
+eval_limit=158
+
+save_every_k_tasks=1 # after completing k dataset's tasks
 save_every_k_iterations=$((save_every_k_tasks * n_samples / batch_size))
 
-common_name="$MODEL_NAME-temp$temperature-p$top_p-$precision-n$n_samples-batch$batch_size-maxlen$max_length-$lang"
-generations_name="$common_name-generations_multiple-$lang"
-generations_path="$BASE_DIR/$generations_name.json"
+BASE_DIR=./runpod/$MODEL_NAME/$lang/improve/pass@1/t$temperature-p$top_p-k$top_k-batch$batch_size-n$n_samples
+
+mkdir -p $BASE_DIR
+rm -rf /tmp/* /var/tmp/*
+
+common_name="$MODEL_NAME-temp$temperature-p$top_p-k$top_k-$precision-n$n_samples-seed$seed-batch$batch_size-maxlen$max_length-$lang"
+generations_name="$common_name-generations-${limit_start}-${limit}_multiple-$lang"
 
 python main.py --model "$AUTHOR/$MODEL_NAME" \
     --tasks multiple-$lang \
+    --token \
     --max_length_generation $max_length \
     --temperature $temperature \
     --top_p $top_p \
@@ -36,17 +45,24 @@ python main.py --model "$AUTHOR/$MODEL_NAME" \
     --allow_code_execution \
     --trust_remote_code \
     --save_every_k_tasks $save_every_k_iterations \
-    --token \
-    --load_generations_path "$generations_path" \
-    --metric_output_path "$BASE_DIR/$generations_name-evaluation_results.json"
+    --save_generations \
+    --save_generations_path "$BASE_DIR/$common_name-generations-${limit_start}-${limit}.json" \
+    --save_references \
+    --limit_start $limit_start \
+    --limit $limit \
+    --metric_output_path "$BASE_DIR/$generations_name-eval-${eval_limit_start}-${eval_limit}-evaluation_results.json" \
+    --max_memory_per_gpu auto
+
+full_language=java
+generations_path="$BASE_DIR/$generations_name.json"
 
 # BLEU score
-bleu_predictions_path="$BASE_DIR/$common_name-bleu-predictions_multiple-$lang.txt"
+bleu_predictions_path="$(realpath $BASE_DIR)/$common_name-bleu-predictions_multiple-$lang.txt"
 python utils/generations_to_codexglue_bleu.py \
     --load_generations_path "$generations_path" \
     --save_predictions_format_path "$bleu_predictions_path"
 
-bleu_references_path="$BASE_DIR/$common_name-bleu-references_multiple-$lang.jsonl"
+bleu_references_path="$(realpath $BASE_DIR)/$common_name-bleu-references_multiple-$lang.jsonl"
 python utils/human_eval_x_to_codexglue_bleu.py \
     --language $full_language \
     --load_generations_path "$generations_path" \
@@ -57,12 +73,12 @@ python utils/human_eval_x_to_codexglue_bleu.py \
 )
 
 # CodeBLEU score
-codebleu_predictions_path="$BASE_DIR/$common_name-codebleu-predictions_multiple-$lang.txt"
+codebleu_predictions_path="$(realpath $BASE_DIR)/$common_name-codebleu-predictions_multiple-$lang.txt)"
 python utils/generations_to_codexglue_codebleu.py \
     --load_generations_path "$generations_path" \
     --save_predictions_format_path "$codebleu_predictions_path"
 
-codebleu_references_path="$BASE_DIR/$common_name-codebleu-references_multiple-$lang.txt"
+codebleu_references_path="$(realpath $BASE_DIR)/$common_name-codebleu-references_multiple-$lang.txt)"
 python utils/human_eval_x_to_codexglue_codebleu.py \
     --language $full_language \
     --load_generations_path "$generations_path" \
