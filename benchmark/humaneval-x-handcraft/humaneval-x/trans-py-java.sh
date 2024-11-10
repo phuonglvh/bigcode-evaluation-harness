5723a02d0afe35f8f7b63610372ca9a6f508aef6
phuonglvh <hpcqt97@gmail.com>
2024-11-10 09:43:50 +0700

    feat: add gens, eval of benchmark/humaneval-x-handcraft

---
+++ benchmark/humaneval-x-handcraft/humaneval-x/trans-py-java.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -euox
+
+export EVAL_JAVA_EXTRA_CLASSPATH_FOLDER=$PWD/build/java-bin
+
+# Translate code2code
+# py to java
+source_generations_path="$(realpath .)/benchmark/humaneval-x-handcraft/humaneval-x/humaneval-x-handcraft-humaneval_python_java_prompts-translations-0-164.json"
+num_source_generations=164
+# source_lang=py
+
+AUTHOR="THUDM"
+MODEL_NAME="humanmeval-x-handcraft"
+
+# lang=java
+eval_limit_start=0
+eval_limit=$num_source_generations
+
+
+filename=$(basename -- "$source_generations_path")
+# extension="${filename##*.}"
+generations_name="${filename%.*}"
+
+BASE_DIR=./benchmark/$MODEL_NAME/humaneval-x
+
+mkdir -p $BASE_DIR
+# rm -rf /tmp/* /var/tmp/*.json
+rm -rf /var/folders/**/**.json
+
+python main.py --model "$AUTHOR/$MODEL_NAME" \
+    --tasks humanevalx-java \
+    --allow_code_execution \
+    --trust_remote_code \
+    --limit_start $eval_limit_start \
+    --limit $eval_limit \
+    --load_generations_path "$source_generations_path" \
+    --metric_output_path "$BASE_DIR/$generations_name-eval-$eval_limit_start-$eval_limit-evaluation_results.json" >> out.log
