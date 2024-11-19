9dbc788f70e957bef9a7543df2524e2802cb7a88
phuonglvh <hpcqt97@gmail.com>
2024-11-19 14:08:30 +0700

    feat: add prompt enhancer on author

---
+++ benchmark/codegeex-chat-pro/humaneval-x/author/trans-py-java.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euox
+
+export EVAL_JAVA_EXTRA_CLASSPATH_FOLDER=$PWD/build/java-bin
+
+AUTHOR="THUDM"
+MODEL_NAME="codegeex-chat-pro"
+
+# Translate code2code
+# py to java
+prompt_version='v001'
+
+source_generations_path="$(realpath .)/benchmark/$MODEL_NAME/humaneval-x/author/$MODEL_NAME-humaneval_python_java_prompts_$prompt_version-translations-0-164.json"
+num_source_generations=164
+# source_lang=py
+
+
+# lang=java
+eval_limit_start=0
+eval_limit=$num_source_generations
+
+
+filename=$(basename -- "$source_generations_path")
+# extension="${filename##*.}"
+generations_name="${filename%.*}"
+
+BASE_DIR=./benchmark/$MODEL_NAME/humaneval-x
+
+mkdir -p $BASE_DIR
+# rm -rf /tmp/* /var/tmp/*.json
+rm -rf /var/folders/**/**.json
+
+python main.py --model "$AUTHOR/$MODEL_NAME" \
+    --tasks humanevalx-java \
+    --allow_code_execution \
+    --trust_remote_code \
+    --limit_start $eval_limit_start \
+    --limit $eval_limit \
+    --load_generations_path "$source_generations_path" \
+    --metric_output_path "$BASE_DIR/$generations_name-eval-$eval_limit_start-$eval_limit-evaluation_results.json"
