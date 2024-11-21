3d768b635c723872adcf99b1ba2305963e677f1c
phuonglvh <hpcqt97@gmail.com>
2024-11-21 21:46:28 +0700

    feat: add gen, eval v2004

--- benchmark/codegeex-chat-pro/humaneval-x/us/trans-py-java.sh
+++ benchmark/codegeex-chat-pro/humaneval-x/us/trans-py-java.sh
@@ -18,7 +18,8 @@ MODEL_NAME="codegeex-chat-pro"
 # prompt_version='v1008'
 # prompt_version='v1009'
 # prompt_version='v2002'
-prompt_version='v2003'
+# prompt_version='v2003'
+prompt_version='v2004'
 
 source_generations_path="$(realpath .)/benchmark/$MODEL_NAME/humaneval-x/us/$MODEL_NAME-humaneval_python_java_prompts_$prompt_version-translations-0-164.json"
 num_source_generations=164
