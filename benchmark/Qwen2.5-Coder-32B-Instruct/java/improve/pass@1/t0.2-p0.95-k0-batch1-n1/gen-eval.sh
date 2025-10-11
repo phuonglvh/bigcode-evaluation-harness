c2dfc87cf55ce2f83b8ccdd61d71137ef81582e6
phuonglvh <hpcqt97@gmail.com>
2025-10-11 15:13:03 +0700

    feat: init Qwen2.5-Coder-32B-Instruct

---
+++ benchmark/Qwen2.5-Coder-32B-Instruct/java/improve/pass@1/t0.2-p0.95-k0-batch1-n1/gen-eval.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -euox
+
+AUTHOR="codellama"
+MODEL_NAME="CodeLlama-13b-Python-hf"
+
+max_length=1024
+temperature=0.2
+top_k=0
+top_p=0.95
+num_return_sequences=1
+batch_size=$num_return_sequences
+
+n_samples=1 # pass@1 only
+precision=bf16
+lang=java
+
+limit_start=0
+limit=158
+eval_limit_start=0
+eval_limit=158
+
+save_every_k_tasks=1 # after completing k dataset's tasks
+save_every_k_iterations=$((save_every_k_tasks * n_samples / batch_size))
+
+# seed=0
+# seed=5
+seed=10
+# seed=15
+# seed=20
+
+common_name="$MODEL_NAME-temp$temperature-p$top_p-k$top_k-$precision-n$n_samples-seed$seed-batch$batch_size-maxlen$max_length-$lang"
+generations_name="$common_name-generations-${limit_start}-${limit}_multiple-$lang"
+
+BASE_DIR=./benchmark/$MODEL_NAME/$lang/improve/pass@1/t$temperature-p$top_p-k$top_k-batch$batch_size-n$n_samples
+
+mkdir -p $BASE_DIR
+rm -rf /tmp/* /var/tmp/*
+
+python main.py --model "$AUTHOR/$MODEL_NAME" \
+    --tasks multiple-$lang \
+    --max_length_generation $max_length \
+    --temperature $temperature \
+    --top_p $top_p \
+    --top_k $top_k \
+    --seed $seed \
+    --n_samples $n_samples \
+    --batch_size $batch_size \
+    --precision $precision \
+    --allow_code_execution \
+    --trust_remote_code \
+    --save_every_k_tasks $save_every_k_iterations \
+    --save_generations \
+    --save_generations_path "$BASE_DIR/$common_name-generations-${limit_start}-${limit}.json" \
+    --save_references \
+    --limit_start $limit_start \
+    --limit $limit \
+    --metric_output_path "$BASE_DIR/$generations_name-eval-${eval_limit_start}-${eval_limit}-evaluation_results.json" \
+    --max_memory_per_gpu auto
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/phuonglvh/bigcode-evaluation-harness.git
 cd bigcode-evaluation-harness
-git checkout feature/thesis
+git checkout feature/thesis-demo-2
 pip install -r requirements.txt
 bash scripts/install-useful-tools.sh
