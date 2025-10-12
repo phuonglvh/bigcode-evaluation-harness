d83ead16647a918028e9f796a495b70a5c9118f2
phuonglvh <hpcqt97@gmail.com>
2025-10-12 10:00:32 +0700

    feat: make bootstrap clone paper branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/phuonglvh/bigcode-evaluation-harness.git
 cd bigcode-evaluation-harness
-git checkout feature/thesis-demo-2
+git checkout feature/paper
 pip install -r requirements.txt
 bash scripts/install-useful-tools.sh
