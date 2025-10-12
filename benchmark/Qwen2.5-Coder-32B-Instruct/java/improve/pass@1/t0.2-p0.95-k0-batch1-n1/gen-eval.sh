8334902d6de5ffef7710f2b31c5487022a582ac7
phuonglvh <hpcqt97@gmail.com>
2025-10-12 12:26:20 +0700

    fix: update limit values for evaluation in gen-eval.sh

--- benchmark/Qwen2.5-Coder-32B-Instruct/java/improve/pass@1/t0.2-p0.95-k0-batch1-n1/gen-eval.sh
+++ benchmark/Qwen2.5-Coder-32B-Instruct/java/improve/pass@1/t0.2-p0.95-k0-batch1-n1/gen-eval.sh
@@ -17,9 +17,9 @@ precision=bf16
 lang=java
 
 limit_start=0
-limit=158
+limit=164
 eval_limit_start=0
-eval_limit=158
+eval_limit=164
 
 save_every_k_tasks=1 # after completing k dataset's tasks
 save_every_k_iterations=$((save_every_k_tasks * n_samples / batch_size))
